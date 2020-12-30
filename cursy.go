92eea49c1e1ae356b41f5a52bb28060a697c9535
Dana <40013308+d-chell@users.noreply.github.com>
2020-12-29 17:36:31 -0800

    Create cursy.go

---
+++ cursy.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main () {
+
+ // determine os
+ // load gif into memory
+ // spawn background process
+    // draw image on screen in random intervals
+    // annoying music? 
+    // feed mouse input
+
+}
